6ec4984dc11b2e4ba7560309022f7e2e8c496b0a
Maximilian Reichel <reichemn@icloud.com>
2021-09-30 11:07:11 +0200

    added bottom nav bar to home screen

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:startup_namer/utility/be_aware_colors.dart';
 import 'package:startup_namer/utility/data_container.dart';
 import 'package:startup_namer/utility/data_view_layout.dart';
@@ -11,9 +11,8 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> { 
-
-   var customRowTop = Row(children: <Widget>[
+class _HomeScreenState extends State<HomeScreen> {
+  var customRowTop = Row(children: <Widget>[
     Expanded(
       flex: 5,
       child: Container(
@@ -67,119 +66,154 @@ class _HomeScreenState extends State<HomeScreen> {
   ]);
 
   @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-      body: SafeArea(
-
-      child: Column(children: [
-            DataViewLayout(
-              upperHeight: 0.7,
-              lowerHeight: 0.20,
-             // lowerBg: Colors.black12,
-              upperChild: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/background.png"),
-                    fit: BoxFit.contain,
-                    //alignment: Alignment.topCenter,
-                  ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: 15.0, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.grey[300]!,
+              hoverColor: Colors.grey[100]!,
+              gap: 8,
+              activeColor: Colors.black,
+              iconSize: 24,
+              padding:
+              EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: Duration(milliseconds: 400),
+              tabBackgroundColor: Color(BeAwareColors.someblue).withAlpha(120),
+              color: Colors.black,
+              tabs: [
+                GButton(icon: Icons.home_outlined, text: 'Home'),
+                GButton(
+                  icon: Icons.settings_outlined,
+                  text: 'Settings',
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.1,
-                      margin: EdgeInsets.only(left: 20.0, right: 20.0),
-                      child: Text("Some Icons"),
-                      ),
-                    Container(
-                      //color: Colors.black38,
-                      height: MediaQuery.of(context).size.height * 0.28,
-                      margin: EdgeInsets.only(left: 20.0, right: 20.0),
-                      child: customRowTop,
-                    ),
-                    Container(     
-                      //color: Colors.black45,
-                      height: MediaQuery.of(context).size.height * 0.28,
-                      
-                      margin: EdgeInsets.only(left: 20.0, right: 20.0),
-                      child: customRowBottom,
-                    ),
-                  ],
-                ),
-              ),
-
-              lowerChild: ( Row(children: <Widget>[
-                Expanded(
-                  child: Container(
-                      //color: Colors.deepOrangeAccent,
-                      height: MediaQuery.of(context).size.height * 0.28,
-                      width: MediaQuery.of(context).size.width,
-                     
-                      child: Row(
-                          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Expanded(
-                                flex: 1,
-                                child: DataContainer(
-                                    height: 0.7,
-                                    width: 0.9,
-                                    child: IconContainer(
-                                      bgColor: Color(BeAwareColors.someblue),
-                                      borderRadius: 10.0,
-                                      icon: Icons.stacked_line_chart_outlined,
-                                      title: "Air",
-                                      tabIndex: 0,
-                                    ))),
-                            //Text("Some Data")
-
-                            Expanded(
-                                child: DataContainer(
-                                    height: 0.7,
-                                    width: 0.9,
-                                    child: IconContainer(
-                                      bgColor: Color(BeAwareColors.someblue),
-                                      borderRadius: 10.0,
-                                      icon: Icons.stacked_bar_chart_outlined,
-                                      title: "Posture",
-                                      tabIndex: 1,
-                                    ))),
-                            //Text("Some Data")
-
-                            Expanded(
-                                child: DataContainer(
-                                    height: 0.7,
-                                    width: 0.9,
-                                    child: IconContainer(
-                                      bgColor: Color(BeAwareColors.someblue),
-                                      borderRadius: 10.0,
-                                      icon: Icons.data_usage_outlined,
-                                      title: "Light",
-                                      tabIndex: 2,
-                                    )))
-                          ]),
-                      decoration: BoxDecoration(
-                      border: Border(
-                        top: BorderSide(
-                        color: Color(BeAwareColors.etonblue), 
-                        width: 4,
-                        ),
-                      ),
-                    ),
-              )
+                GButton(
+                  icon: Icons.person_outlined,
+                  text: 'Profile',
+                )
+              ],
+              selectedIndex: 0,
+              onTabChange: (index) {
+                // TODO
+              },
             ),
-            ],
-          )
-        )
-       )]
-      )
-      )
-      );
+          ),
+        ),
+      ),
+        body: SafeArea(
+            child: Column(children: [
+      DataViewLayout(
+          upperHeight: 0.69,
+          lowerHeight: 0.18,
+          // lowerBg: Colors.black12,
+          upperChild: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/background.png"),
+                fit: BoxFit.contain,
+                //alignment: Alignment.topCenter,
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                  margin: EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: Text("Some Icons"),
+                ),
+                Container(
+                  //color: Colors.black38,
+                  height: MediaQuery.of(context).size.height * 0.28,
+                  margin: EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: customRowTop,
+                ),
+                Container(
+                  //color: Colors.black45,
+                  height: MediaQuery.of(context).size.height * 0.28,
 
+                  margin: EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: customRowBottom,
+                ),
+              ],
+            ),
+          ),
+          lowerChild: (Stack(children: [
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: Container(
+                  //color: Colors.deepOrangeAccent,
+                  height: MediaQuery.of(context).size.height * 0.28,
+                  width: MediaQuery.of(context).size.width,
 
-          
+                  child: Row(
+                      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                            flex: 1,
+                            child: DataContainer(
+                                height: 0.7,
+                                width: 0.9,
+                                child: IconContainer(
+                                  bgColor: Color(BeAwareColors.someblue),
+                                  borderRadius: 10.0,
+                                  icon: Icons.stacked_line_chart_outlined,
+                                  title: "Air",
+                                  tabIndex: 0,
+                                ))),
+                        //Text("Some Data")
 
+                        Expanded(
+                            child: DataContainer(
+                                height: 0.7,
+                                width: 0.9,
+                                child: IconContainer(
+                                  bgColor: Color(BeAwareColors.someblue),
+                                  borderRadius: 10.0,
+                                  icon: Icons.stacked_bar_chart_outlined,
+                                  title: "Posture",
+                                  tabIndex: 1,
+                                ))),
+                        //Text("Some Data")
+
+                        Expanded(
+                            child: DataContainer(
+                                height: 0.7,
+                                width: 0.9,
+                                child: IconContainer(
+                                  bgColor: Color(BeAwareColors.someblue),
+                                  borderRadius: 10.0,
+                                  icon: Icons.data_usage_outlined,
+                                  title: "Light",
+                                  tabIndex: 2,
+                                )))
+                      ]),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(
+                        color: Color(BeAwareColors.etonblue),
+                        width: 4,
+                      ),
+                    ),
+                  ),
+                ))
+              ],
+            ),
+          ])))
+    ])));
   }
-
 }
